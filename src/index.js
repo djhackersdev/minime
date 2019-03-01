991e09dd3bcd5e5cdc2c26e98553e61d3bf995f5
Tau <tau@nowhere>
2019-03-01 18:51:57 -0500

    Rename Chunithm title server module
    Since we plan to support more than one game there will of course
    be more than one "title server".

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const net = require('net')
 const aimedb = require('./aimedb')
 const billing = require('./billing')
 const startup = require('./startup')
-const title = require('./title')
+const chunithm = require('./chunithm')
 
 const tls = {
     cert: fs.readFileSync('pki/server.pem'),
@@ -16,4 +16,4 @@ const tls = {
 net.createServer(aimedb).listen(22345)
 http.createServer(startup).listen(80)
 https.createServer(tls, billing).listen(8443)
-http.createServer(title).listen(9000)
+http.createServer(chunithm).listen(9000)
