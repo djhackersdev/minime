615bb9b2d7aa2073fd6cc4179897ac78035988d1
Tau <tau@nowhere>
2019-04-04 14:09:23 -0400

    wip map settings load

--- src/idz/encoder/loadProfile2.ts
+++ src/idz/encoder/loadProfile2.ts
@@ -5,6 +5,9 @@ import { LoadProfileResponse2 } from "../response/loadProfile2";
 export function loadProfile2(res: LoadProfileResponse2) {
   const buf = Buffer.alloc(0x0d30);
 
+  buf.writeUInt16LE(12, 0x03c8); // BGM
+  buf.writeUInt32LE(0x3548, 0x03d8); // Settings pack (default: max vol, force 4, corner on)
+
   buf.writeInt16LE(0x0065, 0x0000);
   buf.writeInt32LE(res.profileId, 4 + 0x3b4);
   buf.writeInt16LE(res.lv, 4 + 0x03c8);
