a30c37a794ff72ed45f524f450f640f72ce84fdf
Tau <tau@nowhere>
2019-04-22 00:07:23 -0400

    Fix SaveNewCarRequest

--- src/idz/handler/saveNewCar.ts
+++ src/idz/handler/saveNewCar.ts
@@ -2,10 +2,12 @@ import { SaveNewCarRequest } from "../request/saveNewCar";
 import { SaveNewCarResponse } from "../response/saveNewCar";
 import { World } from "../world";
 
-export function saveNewCar(
+export async function saveNewCar(
   w: World,
   req: SaveNewCarRequest
-): SaveNewCarResponse {
+): Promise<SaveNewCarResponse> {
+  await w.car().saveCar(req.profileId, req.car);
+
   return {
     type: "save_new_car_res",
     status: 0,
