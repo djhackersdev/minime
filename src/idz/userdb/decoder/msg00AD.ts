ef10c1c1917348eb3372d3f49566a8190ea9383d
Tau <tau@nowhere>
2020-03-12 17:46:17 -0400

    idz: Fix userdb msg 00AD block alignment

--- src/idz/userdb/decoder/msg00AD.ts
+++ src/idz/userdb/decoder/msg00AD.ts
@@ -1,7 +1,7 @@
 import { Message00AD } from "../request/msg00AD";
 
 msg00AD.msgCode = 0x00ad;
-msg00AD.msgLen = 0x061c;
+msg00AD.msgLen = 0x0620;
 
 export function msg00AD(buf: Buffer): Message00AD {
   return { type: "msg_00AD_req" };
