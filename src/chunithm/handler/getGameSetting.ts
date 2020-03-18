ac9cf50d28f54ebfb305c323cac8255127dcf0f2
Matt Bilker <me@mbilker.us>
2020-03-18 19:28:32 +0000

    chuni: Fix maintenance timer

--- src/chunithm/handler/getGameSetting.ts
+++ src/chunithm/handler/getGameSetting.ts
@@ -1,3 +1,4 @@
+import { writeDate } from "../proto/base";
 import { Repositories } from "../repo";
 import { GetGameSettingRequest } from "../request/getGameSetting";
 import { GetGameSettingResponse } from "../response/getGameSetting";
@@ -6,13 +7,19 @@ export default async function getGameSetting(
   rep: Repositories,
   req: GetGameSettingRequest
 ): Promise<GetGameSettingResponse> {
+  const rebootStartTime = new Date();
+  rebootStartTime.setHours(rebootStartTime.getHours() - 3);
+
+  const rebootEndTime = new Date();
+  rebootEndTime.setHours(rebootEndTime.getHours() - 2);
+
   return {
     gameSetting: {
       dataVersion: "1",
       isMaintenance: "false",
       requestInterval: "10",
-      rebootStartTime: "0",
-      rebootEndTime: "0",
+      rebootStartTime: writeDate(rebootStartTime),
+      rebootEndTime: writeDate(rebootEndTime),
       isBackgroundDistribute: "false",
       maxCountCharacter: "300",
       maxCountItem: "300",
