b989ad22a2083d39fd8a26d2fa03d3977f8ff517
esterTion <lcz970@126.com>
2020-04-19 23:03:31 +0800

    chuni: fix lost scores for real

--- src/chunithm/handler/getGameSetting.ts
+++ src/chunithm/handler/getGameSetting.ts
@@ -23,7 +23,7 @@ export default async function getGameSetting(
       isBackgroundDistribute: "false",
       maxCountCharacter: "300",
       maxCountItem: "300",
-      maxCountMusic: "300",
+      maxCountMusic: "100",
     },
     isDumpUpload: "false",
     isAou: "false",
