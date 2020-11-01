7cd819b33756d6dbd6dc7e63d4e5cb7c110bdd60
BemaniWitch <emi@bemani.gay>
2020-11-01 11:11:38 -0500

    idz: Add LoadStockerResponse.myChara field

--- src/idz/userdb/encoder/loadStocker.ts
+++ src/idz/userdb/encoder/loadStocker.ts
@@ -1,12 +1,23 @@
 import { encodeBitmap } from "./_bitmap";
 import { LoadStockerResponse } from "../response/loadStocker";
 
-export function loadStocker(res: LoadStockerResponse) {
+export function loadStocker1(res: LoadStockerResponse) {
   const buf = Buffer.alloc(0x00a0);
 
   buf.writeInt16LE(0x00a8, 0x0000);
   buf.writeUInt8(res.status, 0x0002);
   encodeBitmap(res.backgrounds, 0x24).copy(buf, 0x0003);
+
+  return buf;
+}
+
+export function loadStocker2(res: LoadStockerResponse) {
+  const buf = Buffer.alloc(0x00a0);
+
+  buf.writeInt16LE(0x009d, 0x0000);
+  buf.writeUInt8(res.status, 0x0004);
+  encodeBitmap(res.backgrounds, 0x2c).copy(buf, 0x0005);
+  encodeBitmap(res.myChara, 0x98).copy(buf, 0x0031);
 
   return buf;
 }
--- src/idz/userdb/handler/loadStocker.ts
+++ src/idz/userdb/handler/loadStocker.ts
@@ -8,10 +8,12 @@ export async function loadStocker(
 ): Promise<LoadStockerResponse> {
   const profileId = await w.profile().find(req.aimeId, req.version);
   const backgrounds = await w.backgrounds().loadAll(profileId);
+  const myChara = await w.myChara().loadAll(profileId);
 
   return {
     type: "load_stocker_res",
     status: 1,
     backgrounds,
+    myChara,
   };
 }
