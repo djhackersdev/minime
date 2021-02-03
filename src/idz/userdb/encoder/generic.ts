59ec2bf576e58c643e4a035b89646f8c8bc1d084
BemaniWitch <emi@bemani.gay>
2021-02-03 18:14:22 -0500

    idz: Split/fix "generic" response encoding

--- src/idz/userdb/encoder/generic.ts
+++ src/idz/userdb/encoder/generic.ts
@@ -1,10 +1,22 @@
 import { GenericResponse } from "../response/generic";
 
-export function generic(res: GenericResponse) {
+export function generic1(res: GenericResponse) {
   const buf = Buffer.alloc(0x0020);
 
   buf.writeInt16LE(0x0001, 0x0000);
   buf.writeInt32LE(res.status || 0, 0x0004);
+
+  return buf;
+}
+
+export function generic2(res: GenericResponse) {
+  const buf = Buffer.alloc(0x0030);
+
+  buf.writeInt16LE(0x0001, 0x0000);
+  buf.writeInt32LE(res.status || 0, 0x0004);
+
+  // write the AIME ID for createProfile in 2.12, kinda awkward but whatever.
+  buf.writeInt32LE(res.status || 0, 0x0008);
 
   return buf;
 }
