0236d44466766fff0e358f777c6f36dc9147aa1d
b166fe48b91eb09f279b79a4ecf57b2f8f0d3705 <b166fe48b91eb09f279b79a4ecf57b2f8f0d3705@redacted>
2021-02-03 18:18:40 -0500

    idz: Split/fix loadRewardTable encoder

--- src/idz/userdb/encoder/loadRewardTable.ts
+++ src/idz/userdb/encoder/loadRewardTable.ts
@@ -1,9 +1,17 @@
 import { LoadRewardTableResponse } from "../response/loadRewardTable";
 
-export function loadRewardTable(res: LoadRewardTableResponse) {
+export function loadRewardTable1(res: LoadRewardTableResponse) {
   const buf = Buffer.alloc(0x01c0);
 
   buf.writeInt16LE(0x0087, 0x0000);
+
+  return buf;
+}
+
+export function loadRewardTable2(res: LoadRewardTableResponse) {
+  const buf = Buffer.alloc(0x01c0);
+
+  buf.writeInt16LE(0x0080, 0x0000);
 
   return buf;
 }
