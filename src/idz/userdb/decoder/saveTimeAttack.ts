60c089a810a48746adea84538ec5c313720403de
b166fe48b91eb09f279b79a4ecf57b2f8f0d3705 <b166fe48b91eb09f279b79a4ecf57b2f8f0d3705@redacted>
2021-02-03 17:56:53 -0500

    idz: Fix saveTimeAttack codecs

--- src/idz/userdb/decoder/saveTimeAttack.ts
+++ src/idz/userdb/decoder/saveTimeAttack.ts
@@ -49,3 +49,35 @@ saveTimeAttack2.msgLen = 0x0080;
 export function saveTimeAttack2(buf: Buffer): SaveTimeAttackRequest {
   return saveTimeAttack(buf);
 }
+
+saveTimeAttack3.msgCode = 0x0136;
+saveTimeAttack3.msgLen = 0x0080;
+
+export function saveTimeAttack3(buf: Buffer): SaveTimeAttackRequest {
+  return {
+    type: "save_time_attack_req",
+    aimeId: buf.readUInt32LE(0x0008) as AimeId,
+    version: 2,
+    dayNight: buf.readUInt8(0x0058) & 1,
+    payload: {
+      routeNo: (buf.readUInt8(0x0058) >> 1) as RouteNo,
+      timestamp: new Date(buf.readUInt32LE(0x005c) * 1000),
+      flags: buf.readUInt8(0x0060),
+      totalTime: buf.readUInt32LE(0x001c) / 1000,
+      sectionTimes: [
+        buf.readUInt32LE(0x0028) / 1000,
+        buf.readUInt32LE(0x002c) / 1000,
+        buf.readUInt32LE(0x0030) / 1000,
+      ],
+      grade: buf.readUInt8(0x0066),
+      carSelector: buf.readUInt16LE(0x0010) as CarSelector,
+    },
+    field_0002: buf.readUInt16LE(0x0004),
+    field_0008: buf.readUInt32LE(0x000c),
+    field_0012: buf.readUInt8(0x0016),
+    field_0015: buf.readUInt8(0x0019),
+    field_005D: buf.readUInt8(0x0061),
+    field_005E: buf.readUInt16LE(0x0062),
+    field_0060: buf.readUInt16LE(0x0064),
+  };
+}
